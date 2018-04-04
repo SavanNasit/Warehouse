e90a2972fe8f6f790ca8a6423adad2f3c840bdb2
anshul <anshul@accreteglobus.com>
2018-04-04 12:20:35 +0530

    Working on Manage Consignment & Packages

--- app/src/main/java/com/accrete/warehouse/rest/ApiClient.java
+++ app/src/main/java/com/accrete/warehouse/rest/ApiClient.java
@@ -3,7 +3,12 @@ package com.accrete.warehouse.rest;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.io.IOException;
+
+import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -24,10 +29,26 @@ public class ApiClient {
 
     public static Retrofit getClient() {
         if (retrofit == null) {
+            HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+            interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+            OkHttpClient client = new OkHttpClient.Builder()
+                    .addInterceptor(interceptor)
+                    .addNetworkInterceptor(new Interceptor() {
+
+                        @Override
+                        public okhttp3.Response intercept(Interceptor.Chain chain) throws IOException {
+                            Request request = chain.request().newBuilder()
+                                    // .addHeader(Constant.Header, authToken)
+                                    .build();
+                            return chain.proceed(request);
+                        }
+                    }).build();
+
 
             retrofit = new Retrofit.Builder()
                     .addConverterFactory(GsonConverterFactory.create(gson))
                     .baseUrl(BASE_URL)
+                    .client(client)
                     .build();
         }
         return retrofit;
@@ -41,7 +62,6 @@ public class ApiClient {
                 .baseUrl(BASE_URL)
                 .build();
     }
-
 
 
 }
